1a1f3117d1c3e8ae5797aaaab419b81204cc2e91
vlopez <victor.lopez@prontopiso.com>
2022-05-09 17:00:41 +0200

    Refactor: clear testing functions

--- src/Application/Command/User/SignUp/SignUpCommand.php
+++ src/Application/Command/User/SignUp/SignUpCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Application\Command\User\SignUp;
 
 use App\Application\Command\Shared\CommandInterface;
+use App\Domain\Shared\ValueObject\LastName;
 use App\Domain\User\Model\ValueObject\Auth\Credentials;
 use App\Domain\User\Model\ValueObject\Auth\HashedPassword;
 use App\Domain\Shared\ValueObject\Email;
@@ -13,6 +14,7 @@ use App\Domain\Shared\ValueObject\Name;
 final class SignUpCommand implements CommandInterface
 {
     private Name $name;
+    private LastName $lastname;
     private Credentials $credentials;
 
     protected function __construct() {}
@@ -25,6 +27,7 @@ final class SignUpCommand implements CommandInterface
         $command = new self();
 
         $command->name = Name::fromString($name);
+        $command->lastname = LastName::fromString($name);
         $email = Email::fromString($email);
         $hashedPassword = HashedPassword::encode($plainPassword);
         $command->credentials = new Credentials($email, $hashedPassword);
@@ -35,6 +38,11 @@ final class SignUpCommand implements CommandInterface
     public function name(): Name
     {
         return $this->name;
+    }
+
+    public function lastname(): LastName
+    {
+        return $this->lastname;
     }
 
     public function credentials(): Credentials
